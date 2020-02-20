798403b642eeef4d2cf8e4df83099bff382f24e7
Darron B <dadonbay@gmail.com>
2020-02-19 18:37:22 -0600

    registration is stored in the database

--- react-task-app/src/App.js
+++ react-task-app/src/App.js
@@ -4,16 +4,41 @@ import AuthContainer from './AuthContainer'
 class App extends React.Component {
   constructor(props){
     super(props)
+    this.state={
+      loggedIn: false, 
+    }
   }
+register = async (reg) => {
+
+  console.log("this is the register param", reg);
+
+  // const url = process.env.REACT_APP_API_URL + '/api/v1/users/register'
+  const url = 'http://localhost:8000/api/v1/users/register'
+  try {
+    const registerResponse = await fetch(url, {
+      method:'POST',
+      credentials:'include',
+      body:JSON.stringify(reg),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    const registerJson = await registerResponse.json()
+  
+  }catch (err){
+    if (err) {
+      console.error(err);
+    }
+  }
+}
   render(){
-    
     return (
       <div className="App">
-       <AuthContainer />
+       <AuthContainer register={this.register}/>
       </div>
     )
-  }
-  
+  }  
 }
 
 export default App;
--- react-task-app/src/AuthContainer/index.js
+++ react-task-app/src/AuthContainer/index.js
@@ -1,18 +1,58 @@
 import React from 'react'
 
 class AuthContainer extends React.Component {
-	constructor(){
-		super()
+	constructor(props){
+		super(props)
 		this.state = {
 			username: '', 
 			email: '',
 			password: ''
 		}
 	}
-		render(){
-			return(
-			<h1>render this</h1>
-				)
-		}
+handleChange = (event) => {
+	this.setState({
+      [event.target.name]: event.target.value
+
+	})
+}
+handleSubmit = (event) => {//how do I get this 
+	event.preventDefault()
+	console.table("This is handleSubmit");
+	this.props.register(this.state)//define this ƒ in App.js
+}
+render(){
+	console.table(this.state)
+	return(
+		<React.Fragment>
+			<form onSubmit={this.handleSubmit}>
+				<div>
+					<input 
+					type="text" 
+					name="username"
+					value={this.state.username}
+					onChange={this.handleChange}
+					placeholder="username"/>
+				</div>
+				<div>
+					<input 
+					type="text" 
+					name="email"
+					value={this.state.email}
+					onChange={this.handleChange}
+					placeholder="email"/>
+				</div>
+				<div>
+					<input 
+					type="password" 
+					name="password"
+					value={this.state.password}
+					onChange={this.handleChange}
+					placeholder="password"/>
+				</div>
+				<button>Submit</button>
+			</form>
+		</React.Fragment>
+		)
+	}
 }
 export default AuthContainer
